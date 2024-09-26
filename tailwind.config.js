bf8390a84387bc218ae173586a6b318838e2d69e
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-09-26 23:34:56 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "3rem",
+        "2xl": "3rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
